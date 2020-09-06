e40afdb2c10b04961fd2f6d15e67c6fb163b1f10
nachovidalc <ignacio.vidal.c@mail.pucv.cl>
2020-09-06 18:19:30 -0300

    primer archivo python

---
+++ primero.py
@@ -0,0 +1,20 @@
+# Tarea 4
+
+
+print("Inserte el primer numero")
+primer_numero = input()
+
+print("Inserte el segundo numero")
+segundo_numero = input()
+
+suma = int(primer_numero) + int(segundo_numero)
+resta = int(primer_numero) - int(segundo_numero)
+division = float(primer_numero) / float(segundo_numero)
+multiplicacion = int(primer_numero) * int(segundo_numero)
+modulo = int(primer_numero) % int(segundo_numero)
+
+print(f"El resultado de la suma entre {primer_numero} y {segundo_numero} es {suma}")
+print(f"El resultado de la resta entre {primer_numero} y {segundo_numero} es {resta}")
+print(f"El resultado de la division entre {primer_numero} y {segundo_numero} es {division}")
+print(f"El resultado de la multiplicacion entre el {primer_numero} y {segundo_numero} es {multiplicacion}")
+print(f"El resultado del modulo entre {primer_numero} y {segundo_numero} es {modulo}")
